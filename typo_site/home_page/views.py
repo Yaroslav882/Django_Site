51b4325dcaa47baaeda4ccdbd981b035907d4bc6
Yaroslav Nahnii <nagniy_ya@yahoo.com>
2019-03-24 18:34:59 +0200

    Change Password
    Changed the registration field. Put the standard from Django.
    Added form Change password

--- typo_site/home_page/views.py
+++ typo_site/home_page/views.py
@@ -1,21 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-
-from home_page.forms import SignUpForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 def home_page(request):
     return render(request, 'index.html')
 
 def signup(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
             return redirect('index')
     else:
-        form = SignUpForm()
-    return render(request, 'registration/signup.html', {'form': form})
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {
+        'form': form
+    })
