81743fa289078f52b951b6f9e8913bd9dac6226a
Yaroslav Nahnii <nagniy_ya@yahoo.com>
2019-03-27 17:24:39 +0200

    Add Email Form

--- typo_site/home_page/forms.py
+++ typo_site/home_page/forms.py
@@ -4,10 +4,8 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Введите ваше имя.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Введите вашу фамилию.')
-    email = forms.EmailField(max_length=254, help_text='На этот email придет ссылка с подтвержденим!.')
+    email = forms.EmailField(max_length=254)
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2', )
--- typo_site/home_page/views.py
+++ typo_site/home_page/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+
+from home_page.forms import SignUpForm
 
 def home_page(request):
     return render(request, 'index.html')
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'registration/signup.html', {
         'form': form
     })
